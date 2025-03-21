a4ababc6d34a5684ac75299af89bb0960329fb2b
umer987 <159330908+umer987@users.noreply.github.com>
2025-03-22 01:45:02 +0500

    Add files via upload

---
+++ BUBBLESORT.java
@@ -0,0 +1,35 @@
+package dsap1;
+
+
+public class Algo {
+    // Bubble Sort function
+   
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {5,1,6,2,4,3};
+		System.out.println("::::::::::::::::::::::::::::SIMPLE BUBBLE SORT PROGRAM::::::::::::::::::::::::");
+
+		System.out.println("unsorted array");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		int temp;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length-1; j++) {
+				if (a[j] > a[j+1]) {
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		
+		
+		System.out.println("sorted array");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
