c24c56cc88ddc1009f5fa7be685ae3378bd276c9
Umer Shakir <159330908+umer987@users.noreply.github.com>
2026-09-18 14:32:59 +0500

    Update example.java

--- example.java
+++ example.java
@@ -13,3 +13,14 @@ public class Algo {
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
+int temp;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length-1; j++) {
+				if (a[j] > a[j+1]) {
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		
