b27de479134c2c3747ff55446214d82b4133360c
Umer Shakir <159330908+umer987@users.noreply.github.com>
2026-09-18 14:32:46 +0500

    Implement main method for Bubble Sort example
    Added main method to demonstrate Bubble Sort.

--- example.java
+++ example.java
@@ -4,3 +4,12 @@ public class Algo {
     // Bubble Sort function
    
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {5,1,6,2,4,3};
+		System.out.println("::::::::::::::::::::::::::::SIMPLE BUBBLE SORT PROGRAM::::::::::::::::::::::::");
+
+		System.out.println("unsorted array");
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
